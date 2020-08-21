d0fef69a98828e56133847ca892ed6f07f3197dc
bruno <brunoliracz@gmail.com>
2020-08-21 18:09:09 -0300

    criando server registry

---
+++ registry-service-eureka/src/main/java/com/br/registryserviceeureka/RegistryServiceEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.br.registryserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServiceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceEurekaApplication.class, args);
+	}
+
+}
---
+++ registry-service-eureka/src/test/java/com/br/registryserviceeureka/RegistryServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.registryserviceeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
