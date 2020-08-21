dae488fef6bd190c644b03713e1cdd122fda6485
bruno <brunoliracz@gmail.com>
2020-08-21 14:33:01 -0300

    criando projeto Order service

---
+++ order-food-service/src/main/java/com/br/orderfoodservice/OrderFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.orderfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFoodServiceApplication.class, args);
+	}
+
+}
