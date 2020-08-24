1dae6dfc60733d13dfc1251868eeeb5e0aa12659
bruno <brunoliracz@gmail.com>
2020-08-24 11:42:50 -0300

    adicionando serviço no eureka e no cloud-config

--- notification-email/src/main/java/com/br/notificationemail/NotificationEmailApplication.java
+++ notification-email/src/main/java/com/br/notificationemail/NotificationEmailApplication.java
@@ -2,10 +2,12 @@ package com.br.notificationemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableEurekaClient
 public class NotificationEmailApplication {
 
 	public static void main(String[] args) {
